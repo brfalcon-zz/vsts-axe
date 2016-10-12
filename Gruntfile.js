79406d9f6da276420d5cc55d205061ea6c9befdb
Alexandre Campos <alexde@microsoft.com>
2016-10-12 00:25:08 -0300

    add parameter to url and browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
-
+//	var a = 'http://www.tjsp.jus.br/Egov/Conciliacao/Default.aspx?f=2,http://www.tjsp.jus.br/EGov/Segmento/Administracao/Default.aspx?f=3'.split(',')
+	var a = grunt.option('u').split(',')
   // Project configuration.
   grunt.initConfig({
   "axe-webdriver": {
@@ -8,6 +9,27 @@ module.exports = function(grunt) {
 		  browser: "phantomjs"
 				},
       urls: ['http://www.tjsp.jus.br/Egov/Conciliacao/Default.aspx?f=2', 'http://www.tjsp.jus.br/EGov/Segmento/Administracao/Default.aspx?f=3'],
+	  dest: "output.json",
+      junitDest: "output.xml"
+
+    },
+	
+	 IE: {
+      options: {
+		  browser: "ie"
+				},
+      urls: ['http://www.tjsp.jus.br/Egov/Conciliacao/Default.aspx?f=2', 'http://www.tjsp.jus.br/EGov/Segmento/Administracao/Default.aspx?f=3'],
+	  dest: "output.json",
+      junitDest: "output.xml"
+
+    }
+	,
+	
+	 Custom: {
+      options: {
+		  browser: grunt.option('browser')
+				},
+      urls: a,
 	  dest: "output.json",
       junitDest: "output.xml"
 
